4bde7a62a0cdd5e18f39ece5690f59cf75fbad3f
Federico Aloi <federico.aloi@gmail.com>
2014-06-17 20:46:06 -0300

    Muevo el setup del fixture inicial a la App

--- src/main/scala/edu/unq/uis/planificador/wicket/MainPage.scala
+++ src/main/scala/edu/unq/uis/planificador/wicket/MainPage.scala
@@ -1,9 +1,6 @@
 package edu.unq.uis.planificador.wicket
 
 import edu.unq.uis.planificador.domain.Empleado
-import edu.unq.uis.planificador.domain.builders.RecurrentCalendarSpaceBuilder._
-import edu.unq.uis.planificador.domain.calendar.DiaDeSemana._
-import edu.unq.uis.planificador.domain.disponibilidad.Turno
 import edu.unq.uis.planificador.homes.{AbstractCollectionBasedHomeEmpleado, EmpleadosCollectionBasedHome}
 import edu.unq.uis.planificador.wicket.empleado.DisponibilidadEmpleadoPanel
 import edu.unq.uis.planificador.wicket.widgets.grid.columns.CustomActionColumn
@@ -19,22 +16,9 @@ import scala.collection.JavaConverters._
 import scala.collection.mutable
 
 class MainPage extends WebPage {
-  val empleadoHome: AbstractCollectionBasedHomeEmpleado = EmpleadosCollectionBasedHome
-
-  val pedro = new Empleado("Pedro", "Picapiedras", "123134")
-  pedro disponibleLos (Lunes de 14 a 19)
-  pedro disponibleLos (Martes de 16 a 20)
-  pedro restriccionEl "2014-04-25"
-  pedro restriccionEl "2014-04-22"
-  pedro asignar (Turno el "2014-04-21" de 14 a 18)
-  empleadoHome.create(pedro)
-
-  val juana = new Empleado("juana", "Picapiedras", "8795468")
-  juana disponibleLos (Viernes de 9 a 22)
-  juana disponibleLos (Sabado de 16 a 20)
-  empleadoHome.create(juana)
-
   val ROWS_PER_PAGE = 20
+
+  val empleadoHome: AbstractCollectionBasedHomeEmpleado = EmpleadosCollectionBasedHome
   var disponibilidadEmpleadoPanel: DisponibilidadEmpleadoPanel = _
 
   add(new EditableGrid[Empleado, String](
--- src/main/scala/edu/unq/uis/planificador/wicket/WicketScalaApplication.scala
+++ src/main/scala/edu/unq/uis/planificador/wicket/WicketScalaApplication.scala
@@ -2,6 +2,11 @@ package edu.unq.uis.planificador.wicket
 
 import de.agilecoders.wicket.core.Bootstrap
 import de.agilecoders.wicket.core.settings.BootstrapSettings
+import edu.unq.uis.planificador.domain.Empleado
+import edu.unq.uis.planificador.domain.builders.RecurrentCalendarSpaceBuilder._
+import edu.unq.uis.planificador.domain.calendar.DiaDeSemana.{Lunes, Martes, Sabado, Viernes}
+import edu.unq.uis.planificador.domain.disponibilidad.Turno
+import edu.unq.uis.planificador.homes.{AbstractCollectionBasedHomeEmpleado, EmpleadosCollectionBasedHome}
 import org.apache.wicket.protocol.http.WebApplication
 
 class WicketScalaApplication extends WebApplication {
@@ -10,7 +15,25 @@ class WicketScalaApplication extends WebApplication {
 
   override def init() {
     super.init()
+    setupFixture()
     Bootstrap.install(this, new BootstrapSettings())
+  }
+
+  def setupFixture() {
+    val empleadoHome: AbstractCollectionBasedHomeEmpleado = EmpleadosCollectionBasedHome
+
+    val pedro = new Empleado("Pedro", "Picapiedras", "123134")
+    pedro disponibleLos (Lunes de 14 a 19)
+    pedro disponibleLos (Martes de 16 a 20)
+    pedro restriccionEl "2014-04-25"
+    pedro restriccionEl "2014-04-22"
+    pedro asignar (Turno el "2014-04-21" de 14 a 18)
+    empleadoHome.create(pedro)
+
+    val juana = new Empleado("juana", "Picapiedras", "8795468")
+    juana disponibleLos (Viernes de 9 a 22)
+    juana disponibleLos (Sabado de 16 a 20)
+    empleadoHome.create(juana)
   }
 
 }
