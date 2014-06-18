cb7ca38e85313425370b07e40a4b34e3d32d9f28
Federico Aloi <federico.aloi@gmail.com>
2014-06-18 04:15:05 -0300

    Mejorado el fixture inicial

--- src/main/scala/edu/unq/uis/planificador/wicket/WicketScalaApplication.scala
+++ src/main/scala/edu/unq/uis/planificador/wicket/WicketScalaApplication.scala
@@ -5,7 +5,7 @@ import de.agilecoders.wicket.core.settings.BootstrapSettings
 import edu.unq.uis.planificador.domain.Empleado
 import edu.unq.uis.planificador.domain.builders.RecurrentCalendarSpaceBuilder._
 import edu.unq.uis.planificador.domain.calendar.DiaDeSemana
-import edu.unq.uis.planificador.domain.calendar.DiaDeSemana.{Lunes, Martes, Sabado, Viernes}
+import edu.unq.uis.planificador.domain.calendar.DiaDeSemana._
 import edu.unq.uis.planificador.domain.disponibilidad.Turno
 import edu.unq.uis.planificador.homes.{AbstractCollectionBasedHomeEmpleado, EmpleadosCollectionBasedHome}
 import edu.unq.uis.planificador.wicket.converters.{DateTimeConverter, DiaDeSemanaConverter}
@@ -36,15 +36,17 @@ class WicketScalaApplication extends WebApplication {
 
     val pedro = new Empleado("Pedro", "Picapiedras", "123134")
     pedro disponibleLos (Lunes de 14 a 19)
-    pedro disponibleLos (Martes de 16 a 20)
-    pedro restriccionEl "2014-04-25"
-    pedro restriccionEl "2014-04-22"
-    pedro asignar (Turno el "2014-04-21" de 14 a 18)
+    pedro disponibleLos (Miercoles de 16 a 20)
+    pedro restriccionEl "2014-06-22"
+    pedro restriccionEl "2014-06-23"
+    pedro asignar (Turno el "2014-06-18" de 14 a 18)
     empleadoHome.create(pedro)
 
-    val juana = new Empleado("juana", "Picapiedras", "8795468")
+    val juana = new Empleado("Juana", "De Arco", "8795468")
+    juana disponibleLos (Miercoles de 9 a 15)
     juana disponibleLos (Viernes de 9 a 22)
     juana disponibleLos (Sabado de 16 a 20)
+    pedro asignar (Turno el "2014-06-18" de 10 a 14)
     empleadoHome.create(juana)
   }
 
